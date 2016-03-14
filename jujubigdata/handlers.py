460690140a29a80e9e6d5b189ef4d74c7ab0e60e
Andrew McLeod <admcleod@gmail.com>
2016-03-14 16:47:57 +0000

    fix zookeeper

--- jujubigdata/handlers.py
+++ jujubigdata/handlers.py
@@ -288,7 +288,7 @@ class HDFS(object):
         self.start_zookeeper()
 
     def stop_zookeeper(self):
-        self._hadoop_daemon('start', 'zkfc')
+        self._hadoop_daemon('stop', 'zkfc')
 
     def start_zookeeper(self):
         self._hadoop_daemon('start', 'zkfc')
